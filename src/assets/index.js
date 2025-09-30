92bef19c2686b5279ec0a4a8eeb98aaaf9bb8549
ranjt12103 <ranjitppatil10@gmail.com>
2025-09-30 17:19:28 +0530

    New Update

--- src/assets/index.js
+++ src/assets/index.js
@@ -148,7 +148,7 @@ const techStackIcons = [
 const expCards = [
   {
     review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
-    imgPath: "/images/exp44.png",
+    imgPath: "/images/exp1.png",
     logoPath: "/images/logo1.png",
     title: "Frontend Developer",
     date: "January 2023 - Present",
